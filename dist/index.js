3a8aba379c4e7668086e2e694ba4ea939d74fd0a
binoyPeries <binoyperies98@gmail.com>
2024-08-13 13:58:27 +0530

    Add more validations

--- dist/index.js
+++ dist/index.js
@@ -37953,8 +37953,9 @@ const yup = __nccwpck_require__(2613);
 
 // constants
 const ALLOWED_COMPONENT_YAML_VERSIONS = ["0.9", "1.0", "1.1"];
-const ALLOWED_TYPES = ["REST", "GraphQL", "GRPC", "TCP", "UDP"];
+const ALLOWED_TYPES = ["REST", "GraphQL", "GRPC", "TCP", "UDP", "WS"];
 const ALLOWED_NETWORK_VISIBILITIES = ["Public", "Private", "Organization"];
+const BASE_PATH_REQUIRED_TYPES = ["REST", "GraphQL", "WS"];
 
 // util functions
 function readComponentYaml(filePath) {
@@ -37963,7 +37964,10 @@ function readComponentYaml(filePath) {
     let fileContent = fs.readFileSync(fullPath, "utf8");
     return fileContent;
   } catch (error) {
-    console.log("No component.yaml found, hence skipping the validation", error.message);
+    console.log(
+      "No component.yaml found, hence skipping the validation:",
+      error.message
+    );
     core.setOutput(
       "No component.yaml found",
       "No component.yaml found, hence skipping the validation"
@@ -37973,7 +37977,7 @@ function readComponentYaml(filePath) {
   }
 }
 
-// validation schemas
+// custom validators
 // checkEndpointNameUniqueness - Custom validation method to check if endpoint names are unique
 yup.addMethod(yup.array, "checkEndpointNameUniqueness", function () {
   return this.test({
@@ -37994,35 +37998,36 @@ yup.addMethod(yup.array, "checkEndpointNameUniqueness", function () {
     },
   });
 });
-// schemaPathRequired - Custom validation method to check when type is REST, schemaFilePath is required
-yup.addMethod(yup.string, "schemaPathRequired", function () {
+// basePathRequired - Custom validation method to check base path is required for REST, GraphQL, and WS endpoints
+yup.addMethod(yup.object, "basePathRequired", function () {
   return this.test({
-    name: "schema-path-required",
+    name: "base-path-required",
     test: (value, testCtx) => {
       const { type } = testCtx.parent;
-      if (type == "REST" && !value) {
+      if (BASE_PATH_REQUIRED_TYPES.includes(type) && !value.basePath) {
         return new yup.ValidationError(
-          "Schema file path is required for REST endpoints"
+          "base path is required for REST, GraphQL, and WS endpoints"
         );
       }
       return true;
     },
   });
 });
+
+// Schema definitions
 // serviceSchema - Schema for service definition
 const serviceSchema = yup
   .object()
   .shape({
-    basePath: yup
-      .string()
-      .required("Missing base path")
-      .matches(/^\/[a-zA-Z0-9\/-]*$/, "Invalid base path"),
+    basePath: yup.string().matches(/^\/[a-zA-Z0-9\/-]*$/, "Invalid base path"),
     port: yup
       .number()
       .required("Missing port number")
-      .moreThan(1000, "Port number must be greater than 1000"),
+      .moreThan(1000, "Port number must be greater than 1000")
+      .lessThan(65535, "Port number must be less than 65535"),
   })
-  .required("Missing service definition");
+  .required("service definition is required")
+  .basePathRequired();
 // endpointSchema - Schema for endpoint definition
 const endpointSchema = yup
   .array()
@@ -38031,17 +38036,21 @@ const endpointSchema = yup
       name: yup
         .string()
         .required("Endpoint name is required")
+        .max(50, "Endpoint name must be less than 50 characters")
         .matches(
           /^[a-z0-9-]+$/,
           "Endpoint name must only contain lowercase letters, digits, and hyphens"
         ),
-      displayName: yup.string().optional(),
+      displayName: yup
+        .string()
+        .required()
+        .max(50, "Display name must be less than 50 characters"),
       service: serviceSchema,
       type: yup.string().required("Missing endpoint type").oneOf(ALLOWED_TYPES),
       networkVisibilities: yup
         .array()
         .of(yup.string().oneOf(ALLOWED_NETWORK_VISIBILITIES)),
-      schemaFilePath: yup.string().schemaPathRequired(),
+      schemaFilePath: yup.string(),
     })
   )
   .checkEndpointNameUniqueness();
@@ -38056,6 +38065,7 @@ let sourceRootDir = "";
 try {
   sourceRootDir = core.getInput("source-root-dir");
 } catch (error) {
+  console.log("Failed to read input:", error.message);
   core.setFailed("Failed to read input", error.message);
 }
 try {
@@ -38069,12 +38079,12 @@ try {
         core.setOutput("Component.yaml validation", "Successful");
       })
       .catch((err) => {
-        console.log("Component.yaml validation failed", err.errors);
-        core.setFailed("Component.yaml validation failed: ", err.errors);
+        console.log("Component.yaml validation failed:", err.errors);
+        core.setFailed("Component.yaml validation failed", err.errors);
       });
   }
 } catch (error) {
-  console.log("Failed to validate component.yaml", error.message);
+  console.log("Failed to validate component.yaml:", error.message);
   core.setFailed("Failed to validate component.yaml", error.message);
 }
 
--- index.js
+++ index.js
@@ -6,8 +6,9 @@ const yup = require("yup");
 
 // constants
 const ALLOWED_COMPONENT_YAML_VERSIONS = ["0.9", "1.0", "1.1"];
-const ALLOWED_TYPES = ["REST", "GraphQL", "GRPC", "TCP", "UDP"];
+const ALLOWED_TYPES = ["REST", "GraphQL", "GRPC", "TCP", "UDP", "WS"];
 const ALLOWED_NETWORK_VISIBILITIES = ["Public", "Private", "Organization"];
+const BASE_PATH_REQUIRED_TYPES = ["REST", "GraphQL", "WS"];
 
 // util functions
 function readComponentYaml(filePath) {
@@ -16,7 +17,10 @@ function readComponentYaml(filePath) {
     let fileContent = fs.readFileSync(fullPath, "utf8");
     return fileContent;
   } catch (error) {
-    console.log("No component.yaml found, hence skipping the validation", error.message);
+    console.log(
+      "No component.yaml found, hence skipping the validation:",
+      error.message
+    );
     core.setOutput(
       "No component.yaml found",
       "No component.yaml found, hence skipping the validation"
@@ -26,7 +30,7 @@ function readComponentYaml(filePath) {
   }
 }
 
-// validation schemas
+// custom validators
 // checkEndpointNameUniqueness - Custom validation method to check if endpoint names are unique
 yup.addMethod(yup.array, "checkEndpointNameUniqueness", function () {
   return this.test({
@@ -47,35 +51,36 @@ yup.addMethod(yup.array, "checkEndpointNameUniqueness", function () {
     },
   });
 });
-// schemaPathRequired - Custom validation method to check when type is REST, schemaFilePath is required
-yup.addMethod(yup.string, "schemaPathRequired", function () {
+// basePathRequired - Custom validation method to check base path is required for REST, GraphQL, and WS endpoints
+yup.addMethod(yup.object, "basePathRequired", function () {
   return this.test({
-    name: "schema-path-required",
+    name: "base-path-required",
     test: (value, testCtx) => {
       const { type } = testCtx.parent;
-      if (type == "REST" && !value) {
+      if (BASE_PATH_REQUIRED_TYPES.includes(type) && !value.basePath) {
         return new yup.ValidationError(
-          "Schema file path is required for REST endpoints"
+          "base path is required for REST, GraphQL, and WS endpoints"
         );
       }
       return true;
     },
   });
 });
+
+// Schema definitions
 // serviceSchema - Schema for service definition
 const serviceSchema = yup
   .object()
   .shape({
-    basePath: yup
-      .string()
-      .required("Missing base path")
-      .matches(/^\/[a-zA-Z0-9\/-]*$/, "Invalid base path"),
+    basePath: yup.string().matches(/^\/[a-zA-Z0-9\/-]*$/, "Invalid base path"),
     port: yup
       .number()
       .required("Missing port number")
-      .moreThan(1000, "Port number must be greater than 1000"),
+      .moreThan(1000, "Port number must be greater than 1000")
+      .lessThan(65535, "Port number must be less than 65535"),
   })
-  .required("Missing service definition");
+  .required("service definition is required")
+  .basePathRequired();
 // endpointSchema - Schema for endpoint definition
 const endpointSchema = yup
   .array()
@@ -84,17 +89,21 @@ const endpointSchema = yup
       name: yup
         .string()
         .required("Endpoint name is required")
+        .max(50, "Endpoint name must be less than 50 characters")
         .matches(
           /^[a-z0-9-]+$/,
           "Endpoint name must only contain lowercase letters, digits, and hyphens"
         ),
-      displayName: yup.string().optional(),
+      displayName: yup
+        .string()
+        .required()
+        .max(50, "Display name must be less than 50 characters"),
       service: serviceSchema,
       type: yup.string().required("Missing endpoint type").oneOf(ALLOWED_TYPES),
       networkVisibilities: yup
         .array()
         .of(yup.string().oneOf(ALLOWED_NETWORK_VISIBILITIES)),
-      schemaFilePath: yup.string().schemaPathRequired(),
+      schemaFilePath: yup.string(),
     })
   )
   .checkEndpointNameUniqueness();
@@ -109,6 +118,7 @@ let sourceRootDir = "";
 try {
   sourceRootDir = core.getInput("source-root-dir");
 } catch (error) {
+  console.log("Failed to read input:", error.message);
   core.setFailed("Failed to read input", error.message);
 }
 try {
@@ -122,11 +132,11 @@ try {
         core.setOutput("Component.yaml validation", "Successful");
       })
       .catch((err) => {
-        console.log("Component.yaml validation failed", err.errors);
-        core.setFailed("Component.yaml validation failed: ", err.errors);
+        console.log("Component.yaml validation failed:", err.errors);
+        core.setFailed("Component.yaml validation failed", err.errors);
       });
   }
 } catch (error) {
-  console.log("Failed to validate component.yaml", error.message);
+  console.log("Failed to validate component.yaml:", error.message);
   core.setFailed("Failed to validate component.yaml", error.message);
 }
